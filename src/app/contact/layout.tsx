4cef05843a3ab3b196e958efce8839369078241f
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2025-09-12 10:33:35 +0200

    init project

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,10 @@
+const ContactLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <p>Contact Layout</p>
+      {children}
+    </div>
+  );
+};
+
+export default ContactLayout;
