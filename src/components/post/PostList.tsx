e77884e4598365d9e9a2624db0e0da9a0a769174
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2025-10-24 10:25:45 +0200

    context

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { usePosts } from "@/hook/usePosts";
+import { PostContext } from "@/context/post.context";
 import PostCard from "./PostCard";
+import { useContext } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 const PostList = () => {
-  const { posts } = usePosts();
+  // call state post from context
+  const { posts, isPending } = useContext(PostContext);
 
   return (
     <div className="container mx-auto">
       <h1>Post List Component</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {isPending && (
+          <>
+            <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+          </>
+        )}
+
         {posts.map((post) => (
           <PostCard post={post} key={post.id} />
         ))}
--- src/context/post.context.tsx
+++ src/context/post.context.tsx
@@ -1,22 +1,55 @@
+"use client";
+
 import { Post } from "@/generated/prisma";
-import { createContext, useState } from "react";
+import postService from "@/services/post.service";
+import { createContext, useEffect, useState } from "react";
 
 interface PostContextType {
   posts: Post[];
   fetchPosts: () => Promise<void>;
   deletePost: (id: string) => Promise<void>;
+  isPending: boolean;
 }
 
-const PostContext = createContext<PostContextType>({
+export const PostContext = createContext<PostContextType>({
   posts: [],
   fetchPosts: async () => {},
   deletePost: async () => {},
+  isPending: false,
 });
 
 export const PostProvider = ({ children }: { children: React.ReactNode }) => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    setIsPending(true);
+    try {
+      const data = await postService.fetchPosts();
+      setPosts(data);
+      setIsPending(false);
+    } catch (error) {
+      console.error(error);
+      setIsPending(false);
+    }
+  };
+
+  const deletePost = async (id: string) => {
+    try {
+      await postService.deletePost(id);
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <PostContext.Provider value={{ posts }}>{children}</PostContext.Provider>
+    <PostContext.Provider value={{ posts, fetchPosts, deletePost, isPending }}>
+      {children}
+    </PostContext.Provider>
   );
 };
